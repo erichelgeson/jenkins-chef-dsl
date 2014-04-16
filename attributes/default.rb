60f2d2d3f455fc8bb5880a97a8240aafbda6c2bb
Eric Helgeson <erichelgeson@gmail.com>
2014-04-16 10:00:27 -0500

    No auth by default, let user decide

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
 # Suffix of the recipe that implements your auth.
 # Eg: github, ldap, etc
-default['jenkins-chef']['auth']                  = 'github'
-default['jenkins-chef']['AuthorizationStrategy'] = 'FullControlOnceLoggedInAuthorizationStrategy'
+default['jenkins-chef']['auth']                  = nil #eg: 'github', 'basic'
+default['jenkins-chef']['AuthorizationStrategy'] = nil #eg: 'FullControlOnceLoggedInAuthorizationStrategy'
 
-# Github auth
+# Github auth example:
 #  Create a 'Developer Application' on Github.
 #  https://github.com/settings/applications
 #  Works for Github Enteprise too!
