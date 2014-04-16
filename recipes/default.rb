48a39f7d4c2ebcd4be83935d3dc49d7488c8b90d
Eric Helgeson <erichelgeson@gmail.com>
2014-04-16 10:03:59 -0500

    Install java by default and add documentation

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,18 +4,28 @@
 #
 
 include_recipe 'chef-sugar::default'
+
+# The jenkins cookbook comes with a very simple java installer. If you need more
+#  complex java installs you are on your own.
+#  https://github.com/opscode-cookbooks/jenkins#java
+include_recipe 'jenkins::java'
+
+# There is a bug in the latest Jenkins that breaks the api/ssh key auth.
+# Also you can not pin packages using apt/yum with Jenkins repo
+# So we opt for the war install and pin to 1.555
+# * https://issues.jenkins-ci.org/browse/JENKINS-22346
+# * https://github.com/opscode-cookbooks/jenkins/issues/170
 node.set['jenkins']['master']['install_method'] = 'war'
 node.set['jenkins']['master']['version'] = '1.555'
 node.set['jenkins']['master']['source'] = "#{node['jenkins']['master']['mirror']}/war/#{node['jenkins']['master']['version']}/jenkins.war"
 include_recipe 'jenkins::master'
 
-# Plugins need to be installed before adding auth to the API.
+# Install any required plugins.
 include_recipe 'jenkins-chef-dsl::plugins'
 include_recipe 'jenkins-chef-dsl::auth'
-package 'git'
 
 # Initial jenkinds-dsl-bootstrap job
-#  This job will be the only job created by hand and will create all other jobs.
+#  This job will be the only job created "by hand" and will create all other jobs.
 #  In this example, we just have the jobs staticly defined in a xml file with a
 #  DSL Groovy script. In practice you would specify a Git repo and build poll/webhook that.
 # Currently 'jenkins_job' takes a file on the file system. In the future it may
