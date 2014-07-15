82f06f2409b19b3defe84832badb8d604e221891
Eric Helgeson <erichelgeson@gmail.com>
2014-07-15 10:15:06 -0500

    update to 0.2 chefdk

--- recipes/chefdk.rb
+++ recipes/chefdk.rb
@@ -1,10 +1,23 @@
-# in the future, maybe use this instead of installing Gems.
+require 'chef/sugar'
 
-chefdk_file = 'chefdk_0.1.0-1_amd64.deb'
-remote_file "#{Chef::Config[:file_cache_path]}/#{chefdk_file}" do
-  source "http://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/#{chefdk_file}"
+if ubuntu?
+  chefdk_file = 'chefdk_0.2.0-2_amd64.deb'
+
+  remote_file "#{Chef::Config[:file_cache_path]}/#{chefdk_file}" do
+    source "http://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/#{chefdk_file}"
+  end
+  dpkg_package "#{Chef::Config[:file_cache_path]}/#{chefdk_file}" do
+    action :install
+  end
 end
 
-dpkg_package "#{Chef::Config[:file_cache_path]}/#{chefdk_file}" do
-  action :install
+if rhel?
+  chefdk_file = 'chefdk-0.2.0-1.el6.x86_64.rpm'
+
+  remote_file "#{Chef::Config[:file_cache_path]}/#{chefdk_file}" do
+    source "http://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/#{chefdk_file}"
+  end
+  rpm_package "#{Chef::Config[:file_cache_path]}/#{chefdk_file}" do
+    action :install
+  end
 end
