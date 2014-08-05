07337b8116aaf353087e4d0a493f22c87208d247
Eric Helgeson <erichelgeson@gmail.com>
2014-08-05 11:02:07 -0500

    bump 0.2.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'erichelgeson@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures jenkins-chef-dsl'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.2'
+version          '0.2.3'
 
 depends 'jenkins', '~> 2.0'
 depends 'chef-sugar', '~> 1.2'
