436afeb4e372756350d06bb500d1fd3cf6a1998e
Eric Helgeson <erichelgeson@gmail.com>
2015-04-28 12:00:46 -0500

    Fixes #5 - License

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,17 @@
 name             'jenkins-chef-dsl'
 maintainer       'Eric Helgeson'
 maintainer_email 'erichelgeson@gmail.com'
-license          'All rights reserved'
+license          'Apache 2'
 description      'Installs/Configures jenkins-chef-dsl'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.4'
+version          '0.2.7'
 
 depends 'jenkins', '~> 2.0'
 depends 'chef-sugar', '~> 1.2'
 depends 'apt'
 depends 'artifactory'
 depends 'java', '~> 1.0'
+depends 'selinux_policy', '~> 0.4'
 
 supports 'ubuntu'
 supports 'centos'
