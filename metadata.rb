58dfd61e261a1ef5e5a8b611891f0c8201343edf
Eric Helgeson <erichelgeson@gmail.com>
2015-06-12 10:13:54 -0500

    Version 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'erichelgeson@gmail.com'
 license          'Apache 2'
 description      'Installs/Configures jenkins-chef-dsl'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.7'
+version          '0.3.0'
 
 depends 'jenkins', '~> 2.0'
 depends 'chef-sugar', '~> 1.2'
