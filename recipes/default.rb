44bc03b6951454c9ddc104297d3f97fb4b6f2d99
Eric Helgeson <erichelgeson@gmail.com>
2015-06-12 10:10:37 -0500

    Yay no longer needed to pin to 1.555

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,20 +9,9 @@ include_recipe 'chef-sugar::default'
 #  complex java installs you are on your own.
 #  https://github.com/opscode-cookbooks/jenkins#java
 include_recipe 'jenkins::java'
-
-# There is a bug in the latest Jenkins that breaks the api/ssh key auth.
-# Also you can not pin packages using apt/yum with Jenkins repo
-# So we opt for the war install and pin to 1.555
-# * https://issues.jenkins-ci.org/browse/JENKINS-22346
-# * https://github.com/opscode-cookbooks/jenkins/issues/170
-node.set['jenkins']['master']['install_method'] = 'war'
-node.set['jenkins']['master']['version']  = '1.555'
-node.set['jenkins']['master']['source']   = "#{node['jenkins']['master']['mirror']}/war/#{node['jenkins']['master']['version']}/jenkins.war"
-node.set['jenkins']['master']['checksum'] = '31f5c2a3f7e843f7051253d640f07f7c24df5e9ec271de21e92dac0d7ca19431'
-
 include_recipe 'jenkins::master'
 
-# Install any required plugins.
+# Install any required plugins and setup auth.
 include_recipe 'jenkins-chef-dsl::plugins'
 include_recipe 'jenkins-chef-dsl::auth'
 
@@ -49,4 +38,8 @@ end
 xml = File.join(Chef::Config[:file_cache_path], 'jenkins-job-DSL-bootstrap.xml')
 jenkins_job 'jenkins-job-DSL-bootstrap' do
   config xml
+end
+
+template '/etc/sudoers.d/jenkins-sudoers' do
+  source 'jenkins-sudoers.erb'
 end
