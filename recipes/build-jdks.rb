b21e42e6e2ce643e7e8eb8b815ef4945ada5e5c2
Eric Helgeson <erichelgeson@gmail.com>
2015-06-12 10:13:22 -0500

    No need for jdk6 and set owners to jenkins

--- recipes/build-jdks.rb
+++ recipes/build-jdks.rb
@@ -1,16 +1,25 @@
 directory '/opt/jdks'
 
-java_ark 'oracle-jdk6-x86_64' do
-  url node['java']['jdk']['6']['x86_64']['url']
-  checksum node['java']['jdk']['6']['x86_64']['checksum']
-  app_home '/opt/jdks/oracle-jdk6-x86_64'
-  action :install
+group 'jenkins'
+user 'jenkins' do
+  gid 'jenkins'
 end
+
+#java_ark 'oracle-jdk6-x86_64' do
+#  url node['java']['jdk']['6']['x86_64']['url']
+#  checksum node['java']['jdk']['6']['x86_64']['checksum']
+#  app_home '/opt/jdks/oracle-jdk6-x86_64'
+#  owner 'jenkins'
+#  group 'jenkins'
+#  action :install
+#end
 
 java_ark "oracle-jdk7-x86_64" do
   url node['java']['jdk']['7']['x86_64']['url']
   checksum node['java']['jdk']['7']['x86_64']['checksum']
   app_home '/opt/jdks/oracle-jdk7-x86_64'
+  owner 'jenkins'
+  group 'jenkins'
 end
 
 java_ark "oracle-jdk8-x86_64" do
@@ -18,6 +27,8 @@ java_ark "oracle-jdk8-x86_64" do
   checksum node['java']['jdk']['8']['x86_64']['checksum']
   app_home '/opt/jdks/oracle-jdk8-x86_64'
   action :install
+  owner 'jenkins'
+  group 'jenkins'
 end
 
 # TODO: Automatically add these jdk's to Jenkins configuration.
